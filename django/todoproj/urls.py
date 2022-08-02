826a6b7979868c6f54958ef2afcd6df8b94df1c7
ouadjou <yusf.ouadjou@gmail.com>
2022-08-02 18:57:28 +0100

    v1.2

---
+++ django/todoproj/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from todoapi import views as todoapi_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/task/',todoapi_views.TaskList.as_view(), name='task-list'),
+]
